9fd9eb9abfe7c33598b154482712d61b9216c168
Taras Slovak <slovak.taras@gmail.com>
2021-09-11 15:30:03 +0300

    show more pic form db.json

--- src/js/main.js
+++ src/js/main.js
@@ -15,5 +15,5 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTesxtInputs('[name="name"]');
     checkTesxtInputs('[name="message"]');
-    showMoreStyles('.button-styles','.styles-2');
+    showMoreStyles('.button-styles','#styles .row');
 });
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,4 +1,6 @@
-const forms = (state) => {
+import { postData } from "./services/requests";
+
+const forms = () => {
     const form = document.querySelectorAll('form'),
         inputs = document.querySelectorAll('input'),
         upload = document.querySelectorAll('[name = "upload"]');
@@ -15,15 +17,6 @@ const forms = (state) => {
     const path = {
         designer: 'assets/server.php',
         question: 'assets/question.php'
-    };
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: 'POST',
-            body: data
-        });
-
-        return await res.text();
     };
 
     const clearInputs = () => {
